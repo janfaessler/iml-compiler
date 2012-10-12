d270751100cb8beb8ab707a8d7e16344cf420a47
Michael Kuenzli <michael@kuenzli.eu>
2012-10-13 00:24:57 +0200

    change contructor signature to match the rest

--- src/ch/fhnw/cbip/compiler/scanner/token/Mode.java
+++ src/ch/fhnw/cbip/compiler/scanner/token/Mode.java
@@ -9,7 +9,7 @@ public abstract class Mode extends AbstractToken {
   private final Terminal terminal;
   private final ModeAttribute attribute;
 
-  public Mode(int line, Terminal terminal, ModeAttribute attribute) {
+  public Mode(Terminal terminal, ModeAttribute attribute, int line) {
     super(terminal, line);
     this.terminal = terminal;
     this.attribute = attribute;
@@ -26,7 +26,7 @@ public abstract class Mode extends AbstractToken {
 
   public static class ChangeMode extends Mode {
     public ChangeMode(ModeAttribute attribute, int line) throws LexicalError {
-      super(line, Terminal.CHANGEMODE, attribute);
+      super(Terminal.CHANGEMODE, attribute, line);
       if ((attribute != ModeAttribute.CONST) && (attribute != ModeAttribute.VAR))
         throw new LexicalError("Invalid ChangeMode attribute", line);
     }
@@ -35,7 +35,7 @@ public abstract class Mode extends AbstractToken {
 
   public static class FlowMode extends Mode {
     public FlowMode(ModeAttribute attribute, int line) throws LexicalError {
-      super(line, Terminal.FLOWMODE, attribute);
+      super(Terminal.FLOWMODE, attribute, line);
       if ((attribute != ModeAttribute.IN) && (attribute != ModeAttribute.OUT)
           && (attribute != ModeAttribute.INOUT))
         throw new LexicalError("Invalid FlowMode attribute", line);
@@ -44,7 +44,7 @@ public abstract class Mode extends AbstractToken {
 
   public static class MechMode extends Mode {
     public MechMode(ModeAttribute attribute, int line) throws LexicalError {
-      super(line, Terminal.MECHMODE, attribute);
+      super(Terminal.MECHMODE, attribute, line);
       if ((attribute != ModeAttribute.COPY) && (attribute != ModeAttribute.REF))
         throw new LexicalError("Invalid MechMode attribute", line);
     }
