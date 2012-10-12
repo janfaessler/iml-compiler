a17dc9d1d183a7129c53b2ebe27e76cfb46d89cd
Michael Kuenzli <michael@kuenzli.eu>
2012-10-12 13:55:31 +0200

    added toString

--- ch/fhnw/cbip/compiler/scanner/Terminal.java
+++ ch/fhnw/cbip/compiler/scanner/Terminal.java
@@ -1,13 +1,34 @@
 package ch.fhnw.cbip.compiler.scanner;
 
 public enum Terminal {
-    LPAREN, RPAREN, LBRACE, RBRACE, 
-    COMMA, SEMICOLON, COLON, QUESTMARK, EXCLAMARK, 
-    MULTOPR, ADDOPR, RELOPR, BECOMES,
-    
-    LITERAL, IDENT,
-    
-    PROGRAM, TYPE, CALL, BOOLOPR, NOT, CHANGEMODE, MECHMODE, IF, ELSE, 
-    FUN, GLOBAL, LOCAL, FLOWMODE, INIT, PROC, RETURNS,  WHILE,
-    SKIP;
+  LPAREN("LPAREN"), RPAREN("RPAREN"), LBRACE("LBRACE"), RBRACE("RBRACE"),
+
+  COMMA("COMMA"), SEMICOLON("SEMICOLON"), COLON("COLON"),
+
+  QUESTMARK("QUESTMARK"), EXCLAMARK("EXCLAMARK"), BECOMES("BECOMES"),
+
+  MULTOPR(""), ADDOPR(""), RELOPR(""), BOOLOPR(""),
+
+  LITERAL(""), IDENT(""),
+
+  PROGRAM("PROGRAM"), TYPE("TYPE"), CALL("CALL"), NOT("NOT"),
+
+  IF("IF"), ELSE("ELSE"),
+
+  FLOWMODE(""), CHANGEMODE(""), MECHMODE(""),
+
+  FUN("FUN"), GLOBAL("GLOBAL"), LOCAL("LOCAL"), INIT("INIT"),
+
+  PROC("PROC"), RETURNS("RETURNS"), WHILE("WHILE"), SKIP("SKIP");
+
+  Terminal(String toString) {
+    this.toString = toString;
+  }
+
+  private String toString;
+
+  @Override
+  public String toString() {
+    return toString;
+  }
 }
