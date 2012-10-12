c4f640a935bf4cb803e41f91dcfb8df5e63eb8dd
Michael Kuenzli <michael@kuenzli.eu>
2012-10-12 14:04:21 +0200

    ITokenList as suggested by Lederer

--- ch/fhnw/cbip/compiler/scanner/ITokenList.java
+++ ch/fhnw/cbip/compiler/scanner/ITokenList.java
@@ -1,14 +1,22 @@
-package fhnw.cbip.compiler.scanner;
+package ch.fhnw.cbip.compiler.scanner;
 
 import ch.fhnw.cbip.compiler.scanner.IToken;
 
 public interface ITokenList<T extends IToken> {
-    void add(T token);
-    
-    void reset();
-    
-    
+  /**
+   *  Add a token to the list
+   * @param token
+   */
+  public void add(T token);
   
- 
- 
+  /**
+   * Resets the Token list?!
+   */
+  public void reset();
+  
+  /**
+   * Returns the next Token and then moves the cursor ahead by one token.
+   * @return next Token
+   */
+  public T nextToken();
 }
