7ea3f546d0b2c49f56a1b9bfce6c0dc168998b5e
Michael Kuenzli <michael@kuenzli.eu>
2012-10-12 14:07:24 +0200

    expanded the basic token interface/class

--- ch/fhnw/cbip/compiler/scanner/AbstractToken.java
+++ ch/fhnw/cbip/compiler/scanner/AbstractToken.java
@@ -1,25 +1,40 @@
 package ch.fhnw.cbip.compiler.scanner;
 
-import fhnw.cbip.compiler.scanner.IToken;
-import fhnw.cbip.compiler.scanner.Terminal;
+import ch.fhnw.cbip.compiler.scanner.IToken;
+import ch.fhnw.cbip.compiler.scanner.enums.Terminal;
 
 public abstract class AbstractToken implements IToken {
+  /**
+   * Terminal of this token
+   */
   private final Terminal terminal;
+  
+  /**
+   * Source code line
+   */
   private final int line;
 
+  /**
+   * Creates a token
+   * @param terminal terminal of the token
+   * @param line source code line of the token
+   */
   public AbstractToken(Terminal terminal, int line) {
     this.terminal = terminal;
     this.line = line;
   }
-
+  
+  /**
+   * @return the token's terminal smybol
+   */
   protected Terminal getTerminal() {
     return terminal;
   }
   
-  public int getLine(){
+  public int getLine() {
     return line;
   }
-  
+
   @Override
   public String toString() {
     return terminal.toString();
--- ch/fhnw/cbip/compiler/scanner/IToken.java
+++ ch/fhnw/cbip/compiler/scanner/IToken.java
@@ -1,5 +1,8 @@
 package ch.fhnw.cbip.compiler.scanner;
 
 public interface IToken {
+  /**
+   * @return source code line number of the token
+   */
   public int getLine();
 }
