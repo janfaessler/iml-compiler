9cacc97fb4e572a1838f886631f2f772921b381d
Michael Kuenzli <michael@kuenzli.eu>
2012-10-19 15:41:43 +0200

    added more code to LetterState

--- src/ch/fhnw/cbip/compiler/scanner/state/LetterState.java
+++ src/ch/fhnw/cbip/compiler/scanner/state/LetterState.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 import ch.fhnw.cbip.compiler.error.LexicalError;
 import ch.fhnw.cbip.compiler.scanner.IScannerContext;
 import ch.fhnw.cbip.compiler.scanner.IScannerState;
+import ch.fhnw.cbip.compiler.scanner.enums.Keyword;
 import ch.fhnw.cbip.compiler.scanner.enums.ScannerSymbol;
+import ch.fhnw.cbip.compiler.scanner.enums.Terminal;
 import ch.fhnw.cbip.compiler.scanner.enums.TypeAttribute;
+import ch.fhnw.cbip.compiler.scanner.token.Ident;
+import ch.fhnw.cbip.compiler.scanner.token.Type;
 
 public class LetterState implements IScannerState {
 
@@ -29,12 +33,28 @@ public class LetterState implements IScannerState {
         String letters = new String(Arrays.copyOfRange(c, 0, c.length - 1));
         int line = context.getLineNumber();
         
+        Terminal.valueOf(letters);
+        Keyword k = Keyword.match(letters);
+        if(k == null){
+          // is an identifier
+          context.addToken(new Ident(letters, line));
+          c = Arrays.copyOfRange(c, lastChar, lastChar+1);
+          context.setState(new InitialState());
+        } else {
+          //is a keyword
+          switch(k){
+            case BOOL:
+              context.addToken(new Type(TypeAttribute.BOOL, line));
+              break;
+            // TODO a lot
+          }
+          c = Arrays.copyOfRange(c, lastChar, lastChar+1);
+          context.setState(new InitialState());
+        }
+        
       }
     }
-    
-    
-    // TODO Auto-generated method stub
-    return null;
+    return c;
   }
 
 }
