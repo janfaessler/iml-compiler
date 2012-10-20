b30458bed0cab02267a702eadff962e539ea9a07
Michael Kuenzli <michael@kuenzli.eu>
2012-10-20 14:26:53 +0200

    added scanner logic

--- src/ch/fhnw/cbip/compiler/scanner/Scanner.java
+++ src/ch/fhnw/cbip/compiler/scanner/Scanner.java
@@ -1,29 +1,51 @@
 package ch.fhnw.cbip.compiler.scanner;
 
-public class Scanner implements IScannerContext {
-  private ITokenList tokenList;
-  private IScannerState currentState;
-  
-  public void addToken(IToken token){
-    tokenList.add(token);
-  }
-  
-  public void setState(IScannerState state){
-    currentState = state;
-  }
-  
-  public IScannerState getState(){
-    return currentState;
-  }
-  
-  public Scanner(String input){
-    
-  }
+import java.io.BufferedReader;
+import java.io.IOException;
 
-  @Override
-  public int getLineNumber() {
-    return 0;
-  }
-  
-  
+import ch.fhnw.cbip.compiler.error.LexicalError;
+import ch.fhnw.cbip.compiler.scanner.state.InitialState;
+import ch.fhnw.cbip.compiler.utils.ArrayUtils;
+
+public class Scanner implements IScannerContext {
+    private ITokenList tokenList = null;
+    private IScannerState currentState = null;
+    private int lineNumber = 0;
+
+    public void addToken(IToken token) {
+        if(tokenList != null){
+            tokenList.add(token);
+        }
+    }
+
+    public void setState(IScannerState state) {
+        if(currentState != null){
+            currentState = state;
+        }
+    }
+
+    public IScannerState getState() {
+        return currentState;
+    }
+
+    public ITokenList scan(BufferedReader input) throws IOException, LexicalError {
+        tokenList = new TokenList(); // reset token list
+        currentState = new InitialState();
+        String currentLine = "";
+        char[] currentChar = {};
+        lineNumber = 0;
+        while ((currentLine = input.readLine()) != null) {
+            lineNumber++;
+            for (char c : currentLine.toCharArray()) {
+                currentChar = currentState.handleChar(ArrayUtils.expandCharArray(currentChar, c), this);
+            }
+        }
+        return tokenList;
+    }
+
+    @Override
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
 }
---
+++ src/ch/fhnw/cbip/compiler/utils/ArrayUtils.java
@@ -0,0 +1,17 @@
+package ch.fhnw.cbip.compiler.utils;
+
+public class ArrayUtils {
+    public static char[] concatCharArray(char[] A, char[] B) {
+        char[] C = new char[A.length + B.length];
+        System.arraycopy(A, 0, C, 0, A.length);
+        System.arraycopy(B, 0, C, A.length, B.length);
+        return C;
+    }
+
+    public static char[] expandCharArray(char[] A, char B) {
+        char[] C = new char[A.length + 1];
+        System.arraycopy(A, 0, C, 0, A.length);
+        C[A.length - 1] = B;
+        return C;
+    }
+}
