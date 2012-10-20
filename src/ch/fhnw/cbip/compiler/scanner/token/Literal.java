edcbf6c4fdf02e78d24cd0fe717d26fdda129c86
Michael Kuenzli <michael@kuenzli.eu>
2012-10-20 12:37:05 +0200

    expanded equals method reformatting

--- src/ch/fhnw/cbip/compiler/scanner/token/Literal.java
+++ src/ch/fhnw/cbip/compiler/scanner/token/Literal.java
@@ -4,53 +4,55 @@ import ch.fhnw.cbip.compiler.scanner.enums.BoolVal;
 import ch.fhnw.cbip.compiler.scanner.enums.Terminal;
 
 public class Literal extends AbstractToken {
-  private final int value;
-  private BoolVal bool;
+    private final int value;
+    private BoolVal bool;
 
-  public Literal(int value, int line) {
-    super(Terminal.LITERAL, line);
-    this.value = value;
-  }
-
-  public Literal(BoolVal bool, int line) {
-    super(Terminal.LITERAL, line);
-    this.value = bool.getIntVal();
-    this.bool = bool;
-  }
-
-  @Override
-  public String toString() {
-    if (bool != null) {
-      return "(LITERAL, " + bool.toString() + ")";
-    } else {
-      return "(LITERAL, IntVal " + value + ")";
+    public Literal(int value, int line) {
+        super(Terminal.LITERAL, line);
+        this.value = value;
     }
-  }
 
-  public boolean getBoolVal() {
-    return (value != 0);
-  }
-
-  public int getIntVal() {
-    return value;
-  }
-
-  public boolean isInteger() {
-    return (bool == null);
-  }
-
-  public boolean isBoolean() {
-    return (bool != null);
-  }
-  
-  @Override
-  public boolean equals(Object o){
-    if(o.getClass() == this.getClass()){
-      if(this.value == ((Literal)o).value){
-        return true;
-      }
+    public Literal(BoolVal bool, int line) {
+        super(Terminal.LITERAL, line);
+        this.value = bool.getIntVal();
+        this.bool = bool;
     }
-    return false;
-  }
+
+    @Override
+    public String toString() {
+        if (bool != null) {
+            return "(LITERAL, " + bool.toString() + ")";
+        } else {
+            return "(LITERAL, IntVal " + value + ")";
+        }
+    }
+
+    public boolean getBoolVal() {
+        return (value != 0);
+    }
+
+    public int getIntVal() {
+        return value;
+    }
+
+    public boolean isInteger() {
+        return (bool == null);
+    }
+
+    public boolean isBoolean() {
+        return (bool != null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o.getClass() == this.getClass()) {
+            if (super.equals((AbstractToken) o)) {
+                if (this.value == ((Literal) o).value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
