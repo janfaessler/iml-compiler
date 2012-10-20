0c088f13345802dc33f60ff0b79607d473f78116
Michael Kuenzli <michael@kuenzli.eu>
2012-10-20 12:35:55 +0200

    added equals method, reformatting

--- src/ch/fhnw/cbip/compiler/scanner/token/AbstractToken.java
+++ src/ch/fhnw/cbip/compiler/scanner/token/AbstractToken.java
@@ -4,39 +4,55 @@ import ch.fhnw.cbip.compiler.scanner.IToken;
 import ch.fhnw.cbip.compiler.scanner.enums.Terminal;
 
 public abstract class AbstractToken implements IToken {
-  /**
-   * Terminal of this token
-   */
-  private final Terminal terminal;
-  
-  /**
-   * Source code line
-   */
-  private final int line;
+    /**
+     * Terminal of this token
+     */
+    private final Terminal terminal;
 
-  /**
-   * Creates a token
-   * @param terminal terminal of the token
-   * @param line source code line of the token
-   */
-  public AbstractToken(Terminal terminal, int line) {
-    this.terminal = terminal;
-    this.line = line;
-  }
-  
-  /**
-   * @return the token's terminal symbol
-   */
-  protected Terminal getTerminal() {
-    return terminal;
-  }
-  
-  public int getLine() {
-    return line;
-  }
+    /**
+     * Source code line
+     */
+    private final int line;
 
-  @Override
-  public String toString() {
-    return terminal.toString();
-  }
+    /**
+     * Creates a token
+     * 
+     * @param terminal terminal of the token
+     * @param line source code line of the token
+     */
+    public AbstractToken(Terminal terminal, int line) {
+        this.terminal = terminal;
+        this.line = line;
+    }
+
+    /**
+     * @return the token's terminal symbol
+     */
+    protected Terminal getTerminal() {
+        return terminal;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    @Override
+    public String toString() {
+        return terminal.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        try {
+            AbstractToken cmp = (AbstractToken) o;
+            if (this.terminal == cmp.terminal) {
+                if (this.line == cmp.line) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            // do nothing
+        }
+        return false;
+    }
 }
