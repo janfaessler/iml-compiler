c8383540dc82a3b25e1dbbdedec7223f54c33a55
Michael Kuenzli <michael@kuenzli.eu>
2012-10-26 14:50:29 +0200

    reformatting

--- src/ch/fhnw/cbip/compiler/scanner/TokenList.java
+++ src/ch/fhnw/cbip/compiler/scanner/TokenList.java
@@ -4,57 +4,56 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class TokenList implements ITokenList {
-  private LinkedList<IToken> tokenList;
-  private Iterator<IToken> listIterator;
-  
-  public TokenList(){
-    tokenList = new LinkedList<IToken>();
-  }
-  
-  
-  @Override
-  public void add(IToken token) {
-    if(token != null){
-      tokenList.add(token);       
-    }   
-  }
+    private LinkedList<IToken> tokenList;
+    private Iterator<IToken> listIterator;
 
-  @Override
-  public void reset() {
-    listIterator = null;
-  }
-
-  @Override
-  public IToken nextToken() {
-    if(listIterator == null){
-      listIterator = tokenList.iterator();
+    public TokenList() {
+        tokenList = new LinkedList<IToken>();
     }
-    return listIterator.next();
-  }
-  
-  @Override
-  public boolean equals(Object o){
-      if(o != null){
-          if(this.getClass().equals(o.getClass())){
-              TokenList cmp = (TokenList)o;
-              Iterator<IToken> thisIt = tokenList.iterator();
-              Iterator<IToken> cmpIt = cmp.tokenList.iterator();
-              
-              while(thisIt.hasNext() && cmpIt.hasNext()){
-                  IToken thisToken = thisIt.next();
-                  IToken cmpToken = cmpIt.next();
-                  if(!thisToken.equals(cmpToken))
-                      return false;
-              }
-              if(thisIt.hasNext() || cmpIt.hasNext()){
-                  return false;
-              } else {
-                  return true;
-              }
-              
-          }
-      }
-      return false;
-  }
+
+    @Override
+    public void add(IToken token) {
+        if (token != null) {
+            tokenList.add(token);
+        }
+    }
+
+    @Override
+    public void reset() {
+        listIterator = null;
+    }
+
+    @Override
+    public IToken nextToken() {
+        if (listIterator == null) {
+            listIterator = tokenList.iterator();
+        }
+        return listIterator.next();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o != null) {
+            if (this.getClass().equals(o.getClass())) {
+                TokenList cmp = (TokenList) o;
+                Iterator<IToken> thisIt = tokenList.iterator();
+                Iterator<IToken> cmpIt = cmp.tokenList.iterator();
+
+                while (thisIt.hasNext() && cmpIt.hasNext()) {
+                    IToken thisToken = thisIt.next();
+                    IToken cmpToken = cmpIt.next();
+                    if (!thisToken.equals(cmpToken))
+                        return false;
+                }
+                if (thisIt.hasNext() || cmpIt.hasNext()) {
+                    return false;
+                } else {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
 
 }
