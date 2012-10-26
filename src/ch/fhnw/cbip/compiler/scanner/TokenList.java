03aebebc334833a3dad1879079dcdf2f2ee5967c
Michael Kuenzli <michael@kuenzli.eu>
2012-10-26 14:50:20 +0200

    added equals

--- src/ch/fhnw/cbip/compiler/scanner/TokenList.java
+++ src/ch/fhnw/cbip/compiler/scanner/TokenList.java
@@ -31,5 +31,30 @@ public class TokenList implements ITokenList {
     }
     return listIterator.next();
   }
+  
+  @Override
+  public boolean equals(Object o){
+      if(o != null){
+          if(this.getClass().equals(o.getClass())){
+              TokenList cmp = (TokenList)o;
+              Iterator<IToken> thisIt = tokenList.iterator();
+              Iterator<IToken> cmpIt = cmp.tokenList.iterator();
+              
+              while(thisIt.hasNext() && cmpIt.hasNext()){
+                  IToken thisToken = thisIt.next();
+                  IToken cmpToken = cmpIt.next();
+                  if(!thisToken.equals(cmpToken))
+                      return false;
+              }
+              if(thisIt.hasNext() || cmpIt.hasNext()){
+                  return false;
+              } else {
+                  return true;
+              }
+              
+          }
+      }
+      return false;
+  }
 
 }
